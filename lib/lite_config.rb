5fa9159a44f2e64c41f2ae7b9710965d86668c30
Gabe da Silveira <gabe@websaviour.com>
2013-12-09 15:54:34 +0000

    Import existing LiteConfig code

--- lib/lite_config.rb
+++ lib/lite_config.rb
@@ -1,5 +1,59 @@
 require "lite_config/version"
 
+require 'yaml'
+
 module LiteConfig
-  # Your code goes here...
+  extend self
+
+  def fetch(name)
+    name = name.to_sym
+    @configs ||= {}
+    @configs.key?(name) ? @configs[name] : (@configs[name] = load(name))
+  end
+
+  private
+
+  def load(name)
+    filename = active_config_filename(name)
+    config = YAML.load_file(filename)[app_env]
+    raise "Oops, no #{app_env} config found for #{name} in #{filename}" unless config
+    config
+  end
+
+  def config_path
+    @config_path ||= File.join(app_root, 'config')
+  end
+
+  def config_filename(name)
+    File.join(config_path, name.to_s + '.yml')
+  end
+
+  def local_config_filename(name)
+    config_filename(name).gsub(/.yml$/, '_local.yml')
+  end
+
+  def active_config_filename(name)
+    File.exist?(local_config_filename(name)) ? local_config_filename(name) : config_filename(name)
+  end
+
+  def app_root
+    defined?(Rails) ? Rails.root : `pwd`.strip
+  end
+
+  def app_env
+    @app_env ||=
+    if defined?(Rails)
+      Rails.env
+    elsif ENV['RAILS_ENV']
+      ENV['RAILS_ENV']
+    elsif ENV['RACK_ENV']
+      ENV['RACK_ENV']
+    else
+      'development'
+    end
+  end
+end
+
+def LiteConfig(name)
+  LiteConfig.fetch(name)
 end
